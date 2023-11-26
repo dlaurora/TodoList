bfb2db395f863edb7d8f0be5f52521a4ca1d9b8d
dlaurora <diegolaurora@gmail.com>
2023-11-26 20:21:55 -0300

    Agregar Impedir imput vacio

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -5,14 +5,25 @@ const TaskForm = ({ addTask }) => {
 
   const handleSubmit = e => {
     e.preventDefault();
-    addTask(input);
-    setInput('');
+    // Solo agrega la tarea si el input no está vacío
+    if (input.trim() !== '') {
+      addTask(input);
+      setInput(''); // Limpia el input después de agregar la tarea
+    }
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <input type='text' value={input} onChange={e => setInput(e.target.value)} />
-      <button type='submit'>Add Task</button>
+    <form onSubmit={handleSubmit} className="task-form">
+      <input
+        type="text"
+        className="task-input"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+        placeholder="Enter a task"
+      />
+      <button type="submit" className="task-button">
+        Add Task
+      </button>
     </form>
   );
 };
